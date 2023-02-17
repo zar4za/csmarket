fc72a02d70388eb1358497591a92ea80c8fec2ae
zar4za <zar4z4@yandex.ru>
2023-02-17 13:00:32 +0300

    Реализация EF UsersContext

---
+++ CsMarket.Tests/Repository/UsersContextTests.cs
@@ -0,0 +1,36 @@
+﻿using CsMarket.Market;
+using CsMarket.Repository;
+using CsMarket.Steam;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsMarket.Tests.Repository
+{
+    public class UsersContextTests
+    {
+        private const string TestConnection = "Host=localhost;Port=5432;Database=csmarket;Username=postgres;Password=postgres";
+        private readonly DbContextOptions<UsersContext> _options = new DbContextOptionsBuilder<UsersContext>().UseNpgsql(TestConnection).Options;
+
+        [Fact]
+        public void StoredUserWithSteamId_ShouldGetCorrectValue()
+        {
+            using var context = new UsersContext(_options);
+            context.Database.BeginTransaction();
+
+            var expected = new User(new Guid(), new SteamId(76561199118590847), "TestName");
+
+            context.Users.Add(expected);
+            context.SaveChanges();
+
+            var user = context.Users.Single(x => x.Id == expected.Id);
+
+            Assert.Equal(expected.Id, user.Id);
+            Assert.Equal(expected.Name, user.Name);
+            Assert.Equal(expected.SteamId.SteamId32, user.SteamId.SteamId32);
+        }
+    }
+}
