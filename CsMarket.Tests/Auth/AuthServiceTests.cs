24335d875b0c87ed8966cec2e85da7a70b5fbbb2
zar4za <zar4z4@yandex.ru>
2023-03-01 23:20:19 +0300

    test: auth service invalid openid
    Тест для метода SignInUser при условии, что пришел недействительный ответ от OpenID провайдера.

--- CsMarket.Tests/Auth/AuthServiceTests.cs
+++ CsMarket.Tests/Auth/AuthServiceTests.cs
@@ -66,5 +66,53 @@ namespace CsMarket.Tests.Auth
             var token = service.SignInUser(claims);
             Assert.Equal(expected, token);
         }
+
+        [Fact]
+        public void SignInUser_NotValid_ShouldThrowException()
+        {
+            var steamid = "https://steamcommunity.com/openid/id/76561199367508734";
+            var format = new SteamIdFormatter(steamid);
+            var user = new User(
+                id: Guid.NewGuid(),
+                steamId: format.ToSteamId32(),
+                name: "testname",
+                role: Role.Common);
+            var generator = new JwtTokenGenerator(new JwtSettings()
+            {
+                Audience = "test",
+                ExpirationSeconds = 600,
+                Issuer = "test",
+                Secret = "test_secret_password"
+            });
+            var claims = new Dictionary<string, string>()
+            {
+                { "openid.identity", steamid }
+            };
+
+            var provider = new Mock<IUserSummaryProvider>();
+            provider.Setup(x => x.GetUserSummary(format.ToSteamId64()))
+                .Returns(new UserSummary()
+                {
+                    Name = user.Name,
+                    AvatarUri = "uri",
+                    RegisterTimestamp = (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                });
+
+            var repository = new Mock<IUserRepository>();
+            repository.Setup(x => x.FindUser(format.ToSteamId32(), out user))
+                .Returns(true);
+
+            var challenge = new Mock<IChallengeProvider>();
+            challenge.Setup(x => x.VerifyOwnership(claims))
+                .Returns(false);
+
+            var service = new AuthService(challenge.Object, generator, repository.Object, provider.Object);
+
+
+            Assert.Throws<Exception>(() =>
+            {
+                var token = service.SignInUser(claims);
+            });
+        }
     }
 }
