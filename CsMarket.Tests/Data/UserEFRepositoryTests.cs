d8b112f54c9d66ede89f0bfc285606f1cef159dc
zar4za <zar4z4@yandex.ru>
2023-03-03 14:29:26 +0300

    test: user ef repository tests
    Добавлены тесты для UserContext и UserEFRepository

---
+++ CsMarket.Tests/Data/UserEFRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using CsMarket.Core;
+using CsMarket.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace CsMarket.Tests.Data
+{
+    public class UserEFRepositoryTests
+    {
+        [Fact]
+        public void AddUser_UserNotNull_ShouldSaveChanges()
+        {
+            using var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<UsersContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using var context = new UsersContext(options);
+
+            var user = new User(Guid.NewGuid(), 1000, "testname", Role.Common)
+            {
+                AvatarUri = "http://uri",
+                RegisterTimestamp = 10000
+            };
+
+            var repo = new UserEFRepository(context);
+
+            repo.AddUser(user);
+
+            Assert.Equal(user, context.Users.Single(x => x.Id == user.Id));
+        }
+
+        [Fact]
+        public void FindUser_UserExists_ShouldReturnTrue()
+        {
+            using var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<UsersContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using var context = new UsersContext(options);
+
+            var expected = new User(Guid.NewGuid(), 1000, "testname", Role.Common)
+            {
+                AvatarUri = "http://uri",
+                RegisterTimestamp = 10000
+            };
+            context.Users.Add(expected);
+            context.SaveChanges();
+
+            var repo = new UserEFRepository(context);
+
+            var hasFound = repo.FindUser(expected.SteamId, out User user);
+
+            Assert.True(hasFound);
+            Assert.Equal(expected, user);
+        }
+
+        [Fact]
+        public void FindUser_UserDoesntExist_ShouldReturnFalse()
+        {
+            using var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<UsersContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            var expected = new User(Guid.NewGuid(), 1000, "testname", Role.Common)
+            {
+                AvatarUri = "http://uri",
+                RegisterTimestamp = 10000
+            };
+
+            using var context = new UsersContext(options);
+
+
+            var repo = new UserEFRepository(context);
+
+            var hasFound = repo.FindUser(expected.SteamId, out User user);
+
+            Assert.False(hasFound);
+            Assert.Null(user);
+        }
+    }
+}
---
+++ CsMarket.Tests/Data/UsersContextTests.cs
@@ -0,0 +1,25 @@
+﻿using CsMarket.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace CsMarket.Tests.Data
+{
+    public class UsersContextTests
+    {
+        [Fact]
+        public void Constructor_InMemoryDatabase_UsersNotNull()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<UsersContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using var context = new UsersContext(options);
+
+            Assert.NotNull(context.Users);
+            connection.Dispose();
+        }
+    }
+}
