dbbb5dc2cf7670c98db37223aec08db0ce7c0805
zar4za <zar4z4@yandex.ru>
2023-03-04 17:41:43 +0300

    feat: steam inventory fetching
    Реализован интерфейс IInventoryFactory в классе SteamInventoryFactory для парсинга инвентаря пользователя.

---
+++ CsMarket/Core/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CsMarket.Core
+{
+    public class Item
+    {
+        [JsonPropertyName("assetid")]
+        public long AssetId { get; init; }
+
+        [JsonPropertyName("classid")]
+        public long ClassId { get; init; }
+
+        [JsonPropertyName("instanceid")]
+        public long InstanceId { get; init; }
+
+        [JsonPropertyName("market_hash_name")]
+        public string MarketHashName { get; init; } = string.Empty;
+
+        [JsonPropertyName("icon_url_large")]
+        public string IconUrl { get; init; } = string.Empty;
+    }
+}
---
+++ CsMarket/Steam/Inventory/IInventoryFactory.cs
@@ -0,0 +1,9 @@
+﻿using CsMarket.Core;
+
+namespace CsMarket.Steam.Inventory
+{
+    public interface IInventoryFactory
+    {
+        IEnumerable<Item> GetInventory(long steamId64);
+    }
+}
