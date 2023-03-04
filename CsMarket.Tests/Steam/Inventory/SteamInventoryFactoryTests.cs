ba0a98a9557066d76cda3cae65a73d632297f4c9
zar4za <zar4z4@yandex.ru>
2023-03-04 19:28:03 +0300

    test: steam inventory factory on success
    Добавил тест при успешном фетче json с сервера steam.

---
+++ CsMarket.Tests/Steam/Inventory/SteamInventoryFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using CsMarket.Core;
+using CsMarket.Steam.Inventory;
+using Moq;
+using RichardSzalay.MockHttp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CsMarket.Tests.Steam.Inventory
+{
+    public class SteamInventoryFactoryTests
+    {
+        [Fact]
+        public void GetInventory_SteamReturnsJson_ShouldReturnInventory()
+        {
+            var json = File.ReadAllText("../../../testdata/steam/inventory/SteamReturnsJson.json");
+            Assert.NotNull(json);
+            var expectedJson = File.ReadAllText("../../../testdata/steam/inventory/SteamReturnsJsonExpected.json");
+            Assert.NotNull(expectedJson);
+            var expectedInventory = JsonSerializer.Deserialize<List<Item>>(expectedJson);
+            var handler = new MockHttpMessageHandler();
+            handler.When("https://steamcommunity.com/inventory/76561198106556563/730/2")
+                .Respond("application/json", json);
+            var client = new HttpClient(handler);
+            var clientFactory = new Mock<IHttpClientFactory>();
+            clientFactory.Setup(x => x.CreateClient(It.IsAny<string>()))
+                .Returns(client);
+            var factory = new SteamInventoryFactory(clientFactory.Object);
+
+
+            var inventory = factory.GetInventory(76561198106556563);
+            Assert.NotNull(inventory);
+            Assert.All(inventory, x => expectedInventory.Contains(x));
+        }
+    }
+}
