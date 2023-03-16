050ffa5812b68e2791eacde3aed85afee50c265d
zar4za <zar4z4@yandex.ru>
2023-03-16 19:03:45 +0300

    chore: update config

--- csm-client/next.config.js
+++ csm-client/next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_HOST + '/api/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
