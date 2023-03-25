db1584a16527626cb9cbe49032be17e985da797e
zar4za <zar4z4@yandex.ru>
2023-03-25 18:46:59 +0300

    fix: change logoipsum with pxly  in footer
    Заменил копирайт в футере.

--- csm-client/app/Footer/Footer.jsx
+++ csm-client/app/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 export default function Footer({ transparent }) {
     return <footer className={classNames(styles.footer, { [styles.footer_transparent]: transparent })}>
         <div className={styles.container}>
-            <span className={styles.footer__copyright}>Copyright © LogoIpsum</span>
+            <span className={styles.footer__copyright}>copyright © pxly</span>
             <ul className={styles.footer__info}>
                 <li className={classNames(styles.footer__link, { [styles.footer__link_transparent]: transparent })}>Контакты</li>
                 <li className={classNames(styles.footer__link, { [styles.footer__link_transparent]: transparent })}>Помощь</li>
