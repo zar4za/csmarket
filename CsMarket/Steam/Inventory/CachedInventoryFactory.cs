de92c03730688a0ed7b2941a927847b67b0722c3
zar4za <zar4z4@yandex.ru>
2023-03-27 00:34:26 +0300

    feat: proxy inventory fetcher

---
+++ CsMarket/Steam/Inventory/CachedInventoryFactory.cs
@@ -0,0 +1,25 @@
+﻿using CsMarket.Core;
+using CsMarket.Data;
+
+namespace CsMarket.Steam.Inventory
+{
+    public class CachedInventoryFactory : IInventoryFactory
+    {
+        private readonly IInventoryFactory _factory;
+        private readonly IAssetRepository _repository;
+
+        public CachedInventoryFactory(IInventoryFactory activeFactory, IAssetRepository repository)
+        {
+            _factory = activeFactory;
+            _repository = repository;
+        }
+
+        public IEnumerable<Item> GetInventory(long steamId64)
+        {
+            _repository.GetInventory()
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
