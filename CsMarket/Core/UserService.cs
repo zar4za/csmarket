c2e6f4d510b62df070ccb12d9850a2385941b67d
zar4za <zar4z4@yandex.ru>
2023-03-28 23:34:09 +0300

    feat: user update method
    Обновление данных юзера через UserService.

---
+++ CsMarket/Core/UserService.cs
@@ -0,0 +1,26 @@
+﻿using CsMarket.Data;
+using CsMarket.Data.Entities;
+
+namespace CsMarket.Core
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void UpdateUser(int steamId, string? email = null, string? tradelink = null)
+        {
+            if (!_repository.FindUser(steamId, out User user))
+                throw new NotImplementedException();
+
+            if (email != null)
+                user.Email = email;
+            if (tradelink != null)
+                user.TradeLink = tradelink;
+        }
+    }
+}
