721f66e56a8feefc97251cc8cc8a216b64b73d38
zar4za <zar4z4@yandex.ru>
2023-03-29 00:42:13 +0300

    refactor: removed all repos
    Убрал все репозитории, т.к. это лишний уровень абстракции.

--- CsMarket/Data/IAssetRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using CsMarket.Data.Entities;
-
-namespace CsMarket.Data
-{
-    public interface IAssetRepository
-    {
-        IQueryable<Asset> GetInventory(int steamId32);
-    }
-}
---
+++ CsMarket/Data/IdentityContext.cs
@@ -0,0 +1,15 @@
+﻿using CsMarket.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CsMarket.Data
+{
+    public class IdentityContext : DbContext
+    {
+        public DbSet<User> Users { get; init; } = null!;
+
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
